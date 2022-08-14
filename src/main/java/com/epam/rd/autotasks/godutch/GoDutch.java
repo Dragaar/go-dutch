15ccdffd042f28d30ba15a2ce419cae31d4b379b
Dragaar <rosivanyshyn@gmail.com>
2022-08-14 14:52:37 +0300

    111

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,22 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
-        //Write code here
+        int sum, friends, partOfSum, a = 0;
+        Scanner scanner = new Scanner(System.in);
+        sum = scanner.nextInt();
+        friends = scanner.nextInt();
+
+
+        if (sum<0){System.out.println("Bill total amount cannot be negative");}
+        else if(friends<=0) { System.out.println("Number of friends cannot be negative or zero");}
+        else {
+            a = (sum/100)*10;
+            partOfSum = (sum+a)/friends;
+            System.out.println(partOfSum);
+        }
     }
 }
